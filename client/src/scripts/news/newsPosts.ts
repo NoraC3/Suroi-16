ecb5145ee0c7a4efe8f5d3ffb38603709fbc5074
NAMERIO <natanberhanu22@gmail.com>
2024-04-06 12:49:40 -0500

    new suroi 16

---
+++ client/src/scripts/news/newsPosts.ts
@@ -0,0 +1,16 @@
+import { type NewsPost } from "./newsHelper";
+
+export const news: NewsPost[] = [
+    {
+        date: Date.parse("April/5/2024"),
+        author: "blazeman",
+        title: "Suroi 16",
+        bannerImage: "../img/news/v0.17.0.png",
+        content: `We are finally bringing Surviv Classic Mode to Suroi. If you want to play together or even contribute to this project, join our <a href="https://discord.gg/4t3z6rP9BH" target="_blank">Discord server</a>. Hope you will enjoy this game.<br><br>
+
+This project is inspired by Suroi 15, so shout out to them.
+
+This project is still in progress.
+</ul>`
+    }
+];
